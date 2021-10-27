4666e4e8d958f678df0e05ffc7a5a56836efc634
regensbu at tuxedo1 <Franz.Regensburger@fabr.de>
2021-10-27 11:25:42 +0200

    merged hg ~/GdPI/CodeExamples into ~/git_public_GdP1/CodeExamples

--- CodeExamples/Stack/myname-V2.c
+++ CodeExamples/Stack/myname-V2.c
@@ -6,8 +6,13 @@ int main() {
   int  y = 0x55667788;
 
   printf("Mein Name lautet %s\n", name);
+  printf("addr x:    %p\n", &x);
+  printf("addr y:    %p\n", &y);
+  printf("addr name: %p\n", name);
+  printf("addr main: %p\n", main);
 
   x++; y++;
   name[1] = 0x65;
+  printf("Mein Name lautet %s\n", name);
   return 0;
 }
