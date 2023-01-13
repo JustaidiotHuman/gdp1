6cccd2e5b0c593c43d20326c8f9557ce22c1d534
regensbu at tuxedo1 <Franz.Regensburger@fabr.de>
2023-01-13 18:50:10 +0100

    added nice example for TCO and some CAVEATS of s++

---
+++ CodeExamples/MyStringFunctions/myStrLen_TCO.c
@@ -0,0 +1,82 @@
+// Nachbau der String-Funktionen aus string.h
+
+#include <stdio.h>
+#include <string.h>  // zum Vergleich
+
+#define SUCCESS 0
+#define FAILURE 1
+
+// Berechne die Laenge der Strings
+
+//--
+// Deep recursive
+
+size_t my_strlen_rec(const char * s) {
+  size_t len = 0;
+  if (*s != '\0'){
+    len = 1 + my_strlen_rec(s+1);
+  }
+  return len;
+}
+
+//--
+// Tail recursive
+
+size_t my_strlen_tail_rec_aux(const char * s, size_t len) {
+  if (*s != '\0'){
+    len = my_strlen_tail_rec_aux(s+1, len+1);
+    //len = my_strlen_tail_rec_aux(s++, len++); // possible variant?
+    //len = my_strlen_tail_rec_aux(++s, ++len); // what about this?
+  }
+  return len;
+}
+
+size_t my_strlen_tail_rec(const char * s) {
+  return my_strlen_tail_rec_aux(s,0);
+}
+
+// Loop with pointer
+size_t my_strlen_iter_ptr(const char * s) {
+  size_t len = 0;
+  while (*s != '\0') {
+    s = s+1;
+    len = len+1;
+  }
+  return len;
+}
+
+//--
+// Loop with array index
+
+size_t my_strlen_iter_idx(const char * s) {
+  size_t len = 0;
+  size_t i = 0;
+  while (s[i] != '\0') {
+    i++;
+    len++;
+  }
+  return len;
+}
+
+//--
+// Disatcher for the various versions
+size_t my_strlen(const char * s) {
+  //return my_strlen_rec(s);
+  //return my_strlen_tail_rec(s);
+  return my_strlen_iter_ptr(s);
+  //return my_strlen_iter_idx(s);
+}
+
+int main(void) {
+  // Test my_strlen()
+  {
+    char *s1 = "1234567";
+    char *s2 = "";
+    printf("my_strlen(\"%s\") = %d\tStandard liefert:%d\n",
+        s1, (int) my_strlen(s1), (int) strlen(s1));
+    printf("my_strlen(\"%s\") = %d\tStandard liefert:%d\n",
+        s2, (int) my_strlen(s2), (int) strlen(s2));
+  }
+
+  return SUCCESS;
+}
