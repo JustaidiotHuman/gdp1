b02cdbb779e8c664df35b92ec465f4a9827f540a
JustaidiotHuman <daniel.rami.code@gmail.com>
2024-10-23 15:39:54 +0200

    Added age and name

--- Code/MyName/myName.c
+++ Code/MyName/myName.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main() {
+  int age;
+  char* name;
 
-  printf("hello world");
+  age = 23;
+  name = "Daniel Rami";
+
+  printf("My Name is %c. Ich bin  %d Jahre alt\n", *(name + 1),age);
 
   return 0;
 }
